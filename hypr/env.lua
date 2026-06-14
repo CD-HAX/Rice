2e8fd58ff3ef00f54e9622a9542d505f4fd67403
Argos <147699014+CD-HAX@users.noreply.github.com>
2026-06-14 16:46:52 +0530

    Reorganize environment variable settings in env.lua

--- hypr/env.lua
+++ hypr/env.lua
@@ -21,8 +21,8 @@ hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-
 hl.env("QT_QPA_PLATFORMTHEME_QT6", "qt6ct")
+
 -- TOOLKIT BACKEND VARIABLES --
 
 hl.env("GDK_BACKEND", "wayland")
