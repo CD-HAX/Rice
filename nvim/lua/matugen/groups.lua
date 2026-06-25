0d238218ed29b337dec4d501c2412f2f88f8681f
CD-HAX <ramdarshan133@gmail.com>
2026-06-25 14:55:25 +0530

    neovim now follows matugen colorscheme

---
+++ nvim/lua/matugen/groups.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+local colors = require("matugen.palette")
+
+M.setup = function()
+  return {
+
+    -- Core UI
+    Normal = { fg = colors.fg, bg = colors.bg },
+    Comment = { fg = colors.comment, italic = true },
+    CursorLine = { bg = colors.cursorline },
+    CursorColumn = { bg = colors.cursorcolumn },
+    Cursor = { bg = colors.cursor },
+    LineNr = { fg = colors.linenr },
+    CursorLineNr = { fg = colors.cursorlinenr },
+    Visual = { bg = colors.visual },
+    Search = { fg = colors.fg, bg = colors.search },
+    IncSearch = { fg = colors.fg, bg = colors.incsearch },
+    StatusLine = { fg = colors.fg, bg = colors.statusline },
+    VertSplit = { fg = colors.vertsplit },
+    Folded = { fg = colors.comment, bg = colors.fold },
+    Pmenu = { fg = colors.fg, bg = colors.pmenu },
+    PmenuSel = { fg = colors.fg, bg = colors.pmenu_sel },
+
+    -- Standard Syntax Highlighting
+    Keyword = { fg = colors.keyword, bold = true },
+    String = { fg = colors.string },
+    Function = { fg = colors.Function },
+    Constant = { fg = colors.constant },
+    Type = { fg = colors.type },
+    Number = { fg = colors.number },
+    Boolean = { fg = colors.boolean },
+    Operator = { fg = colors.operator },
+    Identifier = { fg = colors.identifier },
+
+    -- Dashboard (alpha-nvim)
+    SnacksDashboardHeader = { fg = colors.keyword, bold = true },
+    SnacksDashboardButton = { fg = colors.Function },
+    SnacksDashboardButtonShortcut = { fg = colors.number },
+    SnacksDashboardFooter = { fg = colors.comment },
+    Special = { fg = colors.Function },
+    Directory = { fg = colors.search },
+    Question = { fg = colors.boolean },
+
+    -- Treesitter
+    ["@string"] = { fg = colors.string },
+    ["@function"] = { fg = colors.func },
+    ["@keyword"] = { fg = colors.keyword },
+    ["@type"] = { fg = colors.type },
+    ["@constant"] = { fg = colors.constant },
+
+    -- Diagnostics
+    DiagnosticError = { fg = colors.diag_error },
+    DiagnosticWarn = { fg = colors.diag_warn },
+    DiagnosticInfo = { fg = colors.diag_info },
+    DiagnosticHint = { fg = colors.diag_hint },
+  }
+end
+
+return M
---
+++ nvim/lua/matugen/palette.lua
@@ -0,0 +1,59 @@
+return {
+
+  -- color approach
+
+  red            = "#ffb4ab",
+  orange         = "#c3caa9",
+  green          = "#a1d0c6",
+  yellow         = "#bcece2",
+  blue           = "#bbcf82",
+  magenta        = "#434931",
+  cyan           = "#204e47",
+  black          = "#12140d",
+  bright_red     = "#93000a",
+  bright_orange  = "#c3caa9",
+  bright_green   = "#a1d0c6",
+  bright_yellow  = "#bcece2",
+  bright_blue    = "#bbcf82",
+  bright_magenta = "#dfe6c4",
+  bright_cyan    = "#bcece2",
+  white          = "#e3e3d7",
+  gutter_fg      = "#909284",
+  nontext        = "#c6c8b8",
+
+  -- element approach
+
+  bg         = "#0d0f08",
+  fg         = "#e3e3d7",
+  cursorline = "#1f2019",
+  selection  = "#434931",
+  linenr     = "#46483c",
+  comment    = "#c6c8b8",
+
+  -- Syntax
+  keyword   = "#bbcf82",
+  Function  = "#d7eb9b",
+  string    = "#a1d0c6",
+  constant  = "#c3caa9",
+  type      = "#dfe6c4",
+  number    = "#a1d0c6",
+  boolean   = "#ffb4ab",
+  operator  = "#909284",
+  variable  = "#e3e3d7",
+
+  -- UI
+  cursor     = "#bbcf82",
+  visual     = "#46483c",
+  search     = "#c3caa9",
+  statusline = "#34362d",
+  menu_bg    = "#1f2019",
+  menu_sel   = "#3d4c0d",
+  fold       = "#34362d",
+  split      = "#46483c",
+
+  -- Diagnostics
+  diag_error = "#ffb4ab",
+  diag_warn  = "#a1d0c6",
+  diag_info  = "#c3caa9",
+  diag_hint  = "#bbcf82",
+}
