b87cdc752ed5744ead368273d8b8b5640a788732
CD-HAX <ramdarshan133@gmail.com>
2026-07-06 14:31:40 +0530

    added a script for showing battery percentage and seperated that module

---
+++ waybar/scripts/bat.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+device=$(upower -e | while read -r d; do
+  info=$(upower -i "$d")
+  echo "$info" | grep -qi "model:.*compx VXE" &&
+    echo "$info" | grep -q "present:[[:space:]]*yes" &&
+    {
+      echo "$d"
+      break
+    }
+done)
+
+percent=$(upower -i "$device" | awk '/percentage:/ {
+    gsub("%", "")
+    print $2
+}')
+
+if [ "$percent" -ge 95 ]; then
+  icon=""
+  class="full"
+elif [ "$percent" -ge 75 ]; then
+  icon=""
+  class="high"
+elif [ "$percent" -ge 50 ]; then
+  icon=""
+  class="medium"
+elif [ "$percent" -le 30 ]; then
+  icon=""
+  class="low"
+else
+  icon=""
+  class="critical"
+fi
+
+printf '{"text":"Compx VXE  %s  %s%%","class":"%s"}\n' \
+  "$icon" "$percent" "$class"
